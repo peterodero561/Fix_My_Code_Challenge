db08aa5783085576d6021b22ca64618f0ddac0ae
Peter Odero <peterodero561@gmail.com>
2024-02-20 18:44:36 +0300

    fix: error on deleting a node at an index greter than 0

---
+++ 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * delete_dnodeint_at_index - Delete a node at a specific index from a list
+ *
+ * @head: A pointer to the first element of a list
+ * @index: The index of the node to delete
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *current, *temp;
+	unsigned int i;
+	
+	if (*head == NULL)
+		return (-1);
+	
+	current = *head;
+	if (index == 0)
+	{
+		*head = current->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
+		free(current);
+		return (1);
+	}
+	
+	for (i = 0; current != NULL && i < index - 1; i++)
+		current = current->next;
+	
+	if (current == NULL || current->next == NULL)
+		return (-1);
+	
+	temp = current->next->next;
+	free(current->next);
+	current->next = temp;
+	
+	if (temp != NULL)
+		temp->prev = current;
+	return (1);
+}
